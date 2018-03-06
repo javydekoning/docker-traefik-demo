e63ba2f390a67427247ef3bc18b9c7fb285aaf31
Javy de Koning <javydekoning@gmail.com>
2018-03-06 13:51:55 +0100

    Pushing code

---
+++ encrypt-password.sh
@@ -0,0 +1,3 @@
+#This requires apache2 utils (httpd). Bcrypt requires. 2.4 min. 
+htpasswd -nbB -C 13 username YouShouldNotUseThisPassword!
+username:$2y$13$dSXnrizi74xsfKH740Axt..tIiCL16GGpQn4hpX6Wtph15XGVS08u
---
+++ install-deps.sh
@@ -0,0 +1,17 @@
+#Install dependencies
+sudo su 
+yum update -y
+yum install -y python-pip docker
+pip install docker-compose
+
+#Start docker and configure to start on init. 
+service docker start
+chkconfig docker on
+
+#create files and directories
+mkdir /dockerdemo/traefik -p
+touch /dockerdemo/docker-compose.yml /dockerdemo/traefik/traefik.toml /dockerdemo/traefik/acme.json
+chmod 600 acme.json
+
+#verify
+docker info
